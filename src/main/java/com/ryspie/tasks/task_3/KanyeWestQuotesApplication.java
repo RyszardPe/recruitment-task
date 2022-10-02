c4212ef38a0dce98217863e9460a36c16292fcac
Ryszard P <Riczm0ndes@gmail.com>
2022-10-02 18:28:29 +0200

    Initial commit

---
+++ src/main/java/com/ryspie/tasks/task_3/KanyeWestQuotesApplication.java
@@ -0,0 +1,13 @@
+package com.ryspie.tasks.task_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanyeWestQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanyeWestQuotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryspie/tasks/RecruitmentTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryspie.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
